2c98dbc0aee351d5aa281df09367a3f1b6f6e685
Ilia-Yavari <eiliyayavari@gmail.com>
2025-07-22 20:27:01 +0330

    Handle InputMismatch Exception and few improvements

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,47 @@
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        String[][] board = new String[3][3];
-        for (int i = 0; i < board.length; i++) {
-            Arrays.fill(board[i], "  ");
+    static final String X = "❌";
+    static final String O = "⭕";
+    static final String BLANK = "  ";
+    static final String[][] board = new String[3][3];
+    static {
+        for (String[] row : board) {
+            Arrays.fill(row, BLANK);
         }
-        int emptyCount = 9;
+    }
+
+    public static void main(String[] args) {
+        int emptyCells = board.length * board[0].length;
         boolean firsPlayer = true;
 
         Scanner sc = new Scanner(System.in);
-        while (emptyCount > 0 && !Board.checkWin(board)) {
+        while (emptyCells > 0 && !Board.checkWin(board)) {
             System.out.println();
             Board.printBoard(board);
-            System.out.print("Enter row and column: ");
-            String sign = firsPlayer ? "❌" : "⭕";
-            if (Board.makeMove(board, sc.nextInt() - 1, sc.nextInt() - 1, sign)) {
-                emptyCount--;
-                firsPlayer = !firsPlayer;
-            } else {
-                System.out.println("Invalid move");
+            try {
+                System.out.print("Enter row and column: ");
+                int row = sc.nextInt() - 1, column = sc.nextInt() - 1;
+                String sign = firsPlayer ? X : O;
+                if (Board.makeMove(board, row, column, sign)) {
+                    emptyCells--;
+                    firsPlayer = !firsPlayer;
+                } else {
+                    System.out.println("Invalid move");
+                }
+            } catch (InputMismatchException e) {
+                System.err.println("Enter a valid number");
             }
         }
         sc.close();
         Board.printBoard(board);
         if (Board.checkWin(board)) {
             if (firsPlayer) {
-                System.out.println("⭕ Won!");
+                System.out.println(O + " Won!");
             } else {
-                System.out.println("❌ Won!");
+                System.out.println(X + " Won!");
             }
         } else {
             System.out.println("Draw!");
